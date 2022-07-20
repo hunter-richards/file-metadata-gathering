08782b1e93bab254ac27074e6b1c77718e2163ab
Hunter Richards <djrobotnik@gmail.com>
2022-07-20 13:53:44 -0700

    remove updated path style's preceding slash

--- file_metadata.py
+++ file_metadata.py
@@ -64,7 +64,7 @@ print("Contents of working directory:")
 print(os.listdir(os.getcwd()))
 # Change working directory to the newly unzipped folder containing the downloaded files
 # os.chdir('output\sample-files-master') # original (local)
-os.chdir(r"/output/sample-files-master")
+os.chdir(r"output/sample-files-master")
 
 # Create the list of filenames in the folder
 sample_files_list = []
